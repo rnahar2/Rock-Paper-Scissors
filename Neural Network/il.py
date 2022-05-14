8fe78e7db23c9f007c6a3eca1f841c6e14c16b00
SamuelS95 <44183211+SamuelS95@users.noreply.github.com>
2022-05-14 11:36:12 -0500

    Update il.py

--- Neural Network/il.py
+++ Neural Network/il.py
@@ -1,28 +1,25 @@
-from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.models import Sequential, load_model
-from sklearn.metrics import confusion_matrix
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-import matplotlib.pyplot as plt
-from tensorflow import keras
 import tensorflow as tf
-import numpy as np
-import cv2
-import itertools
-import warnings
 import shutil
 import random
-import os.path
 import glob
 import os
 
-# For categorizing images
+# The following program is designed to take in image data and filter through it to separate
+# it into the class we would like our model to predict (rock, paper, scissors).
+# This data is then preprocessed and batched to be feed into a neural network model
+# where is then trained, validated, and tested to predict either of the 3 outputs of
+# rock, paper, or scissors.
 
-os.chdir('imgs')
-if os.path.isdir('train/rock') is False:
-    os.makedirs('train/rock')
-    os.makedirs('train/paper')
+os.chdir('imgs')    # Here we use the os package to change the directory from the current parent
+                    # directory to the folder imgs where all the images that are to be used for
+                    # training, validation, and testing are stored.
+if os.path.isdir('train/rock') is False:    # If folders are not already created for separating our
+    os.makedirs('train/rock')               # images into train, validation, and test categories, we
+    os.makedirs('train/paper')              # are creating them with their respective classifications
     os.makedirs('train/scissors')
     os.makedirs('valid/rock')
     os.makedirs('valid/paper')
@@ -31,14 +28,14 @@ if os.path.isdir('train/rock') is False:
     os.makedirs('test/paper')
     os.makedirs('test/scissors')
 
-    for c in random.sample(glob.glob('rock*'), 100):
-        shutil.move(c, 'train/rock')
-    for c in random.sample(glob.glob('paper*'), 100):
-        shutil.move(c, 'train/paper')
-    for c in random.sample(glob.glob('scissors*'), 100):
-        shutil.move(c, 'train/scissors')
-    for c in random.sample(glob.glob('rock*'), 25):
-        shutil.move(c, 'valid/rock')
+    for c in random.sample(glob.glob('rock*'), 100):        # Here we are using the glob package to
+        shutil.move(c, 'train/rock')                        # feed random image into their respective
+    for c in random.sample(glob.glob('paper*'), 100):       # directories based on the names they are
+        shutil.move(c, 'train/paper')                       # saved as. Images of rock shape must
+    for c in random.sample(glob.glob('scissors*'), 100):    # begin with rock, etc. We feed 100 images
+        shutil.move(c, 'train/scissors')                    # to each training type, 25 to each
+    for c in random.sample(glob.glob('rock*'), 25):         # validation type, and 10 to each test
+        shutil.move(c, 'valid/rock')                        # types
     for c in random.sample(glob.glob('paper*'), 25):
         shutil.move(c, 'valid/paper')
     for c in random.sample(glob.glob('scissors*'), 25):
@@ -50,13 +47,15 @@ if os.path.isdir('train/rock') is False:
     for c in random.sample(glob.glob('scissors*'), 10):
         shutil.move(c, 'test/scissors')
 
-os.chdir('C:\LSU\Spring 2022\EE 4780\Leanring NN')
+os.chdir('C:\LSU\Spring 2022\EE 4780\Leanring NN')          # Returns us to our previous directory
 
-train_path = 'imgs/train'
+train_path = 'imgs/train'       # Set paths to variables
 valid_path = 'imgs/valid'
 test_path = 'imgs/test'
 
-#For preprocessing to get ready for the NN
+# Using the Keras package Image Data Generator we preprocess our image dta into tensors with the
+# directories we are feeding from, the size we are shaping the images to, classes, and the size
+# of the batches. The Image Data Generator returns a tensor.
 
 train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
     .flow_from_directory(directory=train_path, target_size=(224, 224), classes=['rock', 'paper', 'scissors'],
@@ -68,27 +67,20 @@ test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.v
     .flow_from_directory(directory=test_path, target_size=(224, 224), classes=['rock', 'paper', 'scissors'],
                          batch_size=6, shuffle=False)
 
-#To make sure everything saved correctly
-
+# The following helps validate that the batches indeed has the amount of images we intended to pass
+# into the batches and the number of classes they are separated into.
+# Where classes*ImagesPerClass=TotalBatchSize
 
 assert train_batches.n == 300
 assert valid_batches.n == 75
 assert test_batches.n == 30
 assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 3
 
-#imgs, labels = next(train_batches)
-
-
-'''def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
-    axes = axes.flatten()
-    for img, ax in zip(images_arr, axes):
-        ax.imshow(img)
-        ax.axis('off')
-    plt.tight_layout()
-    plt.show()'''
-
-#The Sequential model
+# We are using a Keras Sequential Model that has 2 hidden convolutional layers. Where the first
+# hidden layer has 32 filters, a kernal size of 3x3, activation using the rectified linear, no
+# padding, and accepts a tensor of rows=224, cols=224, and channels=3. We also downsample input,
+# using the 2x2 pool size and strides of 2, to half. The second hidden layer is the same just
+# with double the filters. Data is then flattened and we output to 3 neurons with softmax activation.
 
 model = Sequential([
     Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)),
@@ -99,32 +91,27 @@ model = Sequential([
     Dense(units=3, activation='softmax'),
 ])
 
-'''To check setup
-model.summary()
-'''
+# Used to validate correct model setup
+# model.summary()
 
-#Running the model
+
+# Here we compile the model using the Adam optimizer with a learning rate of 0.0001 and so it calculates
+# loss and accuracy. After the model is compiled we then fit the data from the batch variables and set
+# the epochs and steps accordingly
+#
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=train_batches, validation_data=valid_batches, epochs=20, steps_per_epoch=5)
+model.fit(x=train_batches, validation_data=valid_batches, epochs=20, steps_per_epoch=10)
 
-#For saving to the model to call and insert input
-if os.path.isfile('models/3UC.h5') is False:
-    model.save('models/3UC.h5')
+# Insert result data here
 
-#new_model = load_model('models/3UC.h5')
+# For saving to the model to call for prediction
+model.save('P.model')
 
-'''while True:
-    data = input()
-    if str.lower(data) == "q":
-        break
-    NNin.append(data)'''
 
-'''#How we will pass our input
-nnin = cv2.imread('imgs/paper.jpg')
+# For manually passing an image into the NN. More info in main file.
+'''nnin = cv2.imread('imgs/rock.jpg')
 imgResize = cv2.resize(nnin, (224, 224))
 imgResize = imgResize[np.newaxis, :]
-print(imgResize.shape)
 
 predictions = model.predict(x=imgResize, steps=1, verbose=0)
 print(np.argmax(predictions))'''
-
